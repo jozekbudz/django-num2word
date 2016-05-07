d03e88e37dd6fa0fb568e15de4f8acf2f3b59699
Józef Budziński <jozef.budzinski@interia.pl>
2016-05-07 20:26:01 +0200

    add test by selenium fix url dispatcher

--- num2word/tests.py
+++ num2word/tests.py
@@ -1,3 +1,23 @@
+import unittest
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class TestCase(TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_signup_fire(self):
+        self.driver.get("http://127.0.0.1:8000/num2words/-3231.3454/de/")
+        self.assertIn("http://127.0.0.1:8000/", self.driver.current_url)
+
+    def test_signup_fire2(self):
+        self.driver.get("http://127.0.0.1:8000/num2words/-3231.3454/")
+        self.assertIn("http://127.0.0.1:8000/", self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit
+
+
+if __name__ == '__main__':
+    unittest.main()
