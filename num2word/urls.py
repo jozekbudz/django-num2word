406cc6f82b33774ebe3c6fc9a532250a81be4979
Józef Budziński <jozef.budzinski@interia.pl>
2016-05-07 15:16:30 +0200

    Created simple base functionality urls handlers and simple generic view

---
+++ num2word/urls.py
@@ -0,0 +1,31 @@
+"""untitled URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.http import HttpResponse
+import json
+
+from .views import ConvView
+
+# from django.contrib import admin
+usage = "Usage: main_url/float_number/lang_code/. Example http://example.com/num2words/-37345.66/PL/ or" \
+        " http://127.0.0.1:8000/num2words/-37345.66/"
+
+urlpatterns = [
+    url(r'(?P<number>[-.\w]+)/(?P<lang>\w{2})/$',ConvView.as_view(), name='num2words'),
+    url(r'(?P<number>[-.\w]+)/$',ConvView.as_view(), name='num2words'),
+    url(r'', lambda r: HttpResponse(json.dumps({'error': usage}),  content_type="application/json")),
+    # url(r'^admin/', admin.site.urls),
+]
--- num2word/views.py
+++ num2word/views.py
@@ -1,3 +1,37 @@
+from decimal import Decimal
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+from num2words import num2words
+
+import json
+
+
+class ConvView(View):
+
+    def get(self, request, *args, **kwargs):
+        """
+        Get number and language code from url
+        :return: json HttpResponse
+        """
+
+        try:
+            number = Decimal(self.kwargs['number'], 10) if 'number' in self.kwargs else None
+        except:
+            number = None
+
+        lang = self.kwargs['lang'].lower() if 'lang' in self.kwargs else 'en'
+        res = {'error': 'Wrong url parameters. Example main_url/float_number/lang_code/'}
+
+        if number:
+            try:
+                res = {"number": num2words(number, lang=lang).replace('-', ' ')}
+            except NotImplementedError:
+                res = {
+                    "number": num2words(number, lang='en').replace('-', ' '),
+                    'message': 'Lang not supported used default en'
+                }
+
+        return HttpResponse(json.dumps(res, ensure_ascii=False),  content_type="application/json;charset=utf-8")
